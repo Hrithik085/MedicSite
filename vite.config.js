21248da1fe34eee4b5b22d2555efc4001c0aaeec
hrithik-at-105410518218 <hrithik.pramod@elogosoft.in>
2025-01-17 22:20:28 +0530

    initial commit

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
-import { defineConfig } from 'vite';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
+// https://vite.dev/config/
 export default defineConfig({
-  server: {
-    port: process.env.PORT || 4173,
-    host: '0.0.0.0', // Ensures it binds to all network interfaces
-  },
-});
+  plugins: [react()],
+})
